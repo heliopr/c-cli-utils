cdf0e9fd6f0b9a8fd1ad2b83ad75a326db3c2b1a
heliopr <helioprorep@gmail.com>
2026-05-21 10:36:21 -0300

    feat(mycat): improve mycat cli

--- src/mycat/main.c
+++ src/mycat/main.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE 255
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Error: No files were given.\n");
-        return 1;
+        printf("Usage: %s <file1> [file2 ...]\n", argv[0]);
+        return EXIT_FAILURE;
     }
 
     for (int i = 1; i < argc; i++) {
         FILE *fptr;
-        char buffer[255];
         fptr = fopen(argv[i], "r");
 
-        if (fptr != NULL) {
-            while (fgets(buffer, 255, fptr) != NULL) {
-                printf("%s", buffer);
-            }
-            
-            fclose(fptr);
+        if (fptr == NULL) {
+            fprintf(stderr, "Error opening file '%s': ", argv[i]);
+            perror("");
+            continue;
         }
+
+        char buffer[BUFFER_SIZE];
+        while (fgets(buffer, BUFFER_SIZE, fptr) != NULL) {
+            printf("%s", buffer);
+        }
+        
+        fclose(fptr);
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
