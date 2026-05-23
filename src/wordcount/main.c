078106d57db7c2400d7c771b81b7df1b75553300
heliopr <helioprorep@gmail.com>
2026-05-23 14:11:46 -0300

    feat(wordcount): improve logic

--- src/wordcount/main.c
+++ src/wordcount/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -16,15 +17,18 @@ int main(int argc, char *argv[]) {
     int ch = 0, last_ch = ' ';
     int count = 0;
     while ((ch = fgetc(fptr)) != EOF) {
-        if (ch != ' ' && ch != '\n' && (last_ch == ' ' || last_ch == '\n'))
+        if (!isspace(ch) && isspace(last_ch)) {
             count++;
+        }
         last_ch = ch;
     }
 
     printf("%d\n", count);
 
-    if (fclose(fptr))
+    if (fclose(fptr)) {
         perror("fclose error");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
