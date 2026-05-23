89db3b400dfecd434498879b3b66007f1b91066a
heliopr <helioprorep@gmail.com>
2026-05-23 14:14:35 -0300

    feat(wordcount): add stdin

--- src/wordcount/main.c
+++ src/wordcount/main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+    FILE *fptr = stdin; 
 
-    FILE *fptr = fopen(argv[1], "r");
-    if (fptr == NULL) {
-        perror(argv[1]);
-        return EXIT_FAILURE;
+    if (argc > 1 && strcmp(argv[1], "-") != 0) {
+        fptr = fopen(argv[1], "r");
+        if (fptr == NULL) {
+            perror(argv[1]);
+            return EXIT_FAILURE;
+        }
     }
 
     int ch = 0, last_ch = ' ';
     int count = 0;
+    
     while ((ch = fgetc(fptr)) != EOF) {
         if (!isspace(ch) && isspace(last_ch)) {
             count++;
@@ -25,9 +26,11 @@ int main(int argc, char *argv[]) {
 
     printf("%d\n", count);
 
-    if (fclose(fptr)) {
-        perror("fclose error");
-        return EXIT_FAILURE;
+    if (fptr != stdin) {
+        if (fclose(fptr)) {
+            perror("fclose error");
+            return EXIT_FAILURE;
+        }
     }
 
     return EXIT_SUCCESS;
